318e003f5cfec290ff189361b28b86ea0c16dd2f
zahidkhattak <zahidkhattakcs@gmail.com>
2023-08-21 21:19:52 +0500

    PHP first day work

---
+++ 1st/firstone.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "My first PHP Program";
+
+echo "Echo is used for print something in screen in PHP Programming";
+
+?>
---
+++ 2nd/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Welcome to php 2nd day practice <br><br><br><br>";
+
+
+echo "Welcome to php 2nd day practice <br><br><br><br>";
+
+
+echo "Welcome to php 2nd day practice <br><br><br><br>";
+
+echo "If we are working only in php file not adding other language like html etc than it's up to you if you use closing tag this will be right if you not used it this wil be work like in this document we can not using closing tag";
+
+
+echo "if you emmbed another language or markup language you must be add closing tag at end of the php last statment or last line <br><br><br><br>";
+
+echo "<h1>Note :</h1><br> semicolon is most important at the end of the each line in php just like that 👉 ; "; 
+;
