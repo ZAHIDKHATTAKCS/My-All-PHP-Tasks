f5185a410228f35485addd84be3dfa297a23d716
zahidkhattak <zahidkhattakcs@gmail.com>
2023-08-24 21:19:45 +0500

    Learning of formulas in php today

---
+++ 7th/areas_formulas.php
@@ -0,0 +1,37 @@
+<?php
+
+
+// area of circle formula A= Pi r2
+
+$pi=3.141;
+// radius let's suppose 5
+ $radius=5;
+
+ $area=$pi*$radius*$radius;
+
+ echo "The result is = $area <br> <br>";
+
+
+//  Now area of triangle formula is A= height*base * 1/2
+
+// lets suppose here height is equal = 5 and base = 3
+
+$height=5;
+$base=3;
+
+$areaoftiangle= 1/2*($height*$base);
+
+echo "The Area of Triangle is = $areaoftiangle <br><br>";
+
+
+// Now area of square  the original formula is A=a2
+
+
+
+
+
+
+
+
+
+?>
---
+++ 7th/powrerofnumber.php
@@ -0,0 +1,26 @@
+<?php
+
+    $base = 4;
+    $power=2;
+
+    echo "Base is equal to = $base <br><br>";
+    echo "power is equal to = $power <br><br>";
+
+
+    $powerofnumber=pow($base,$power);
+
+    echo "The result is = $powerofnumber <br> <br> <br>";
+
+
+    $base2=3;
+
+    $power2=2;
+
+    echo "Here Base is = $base2 <br><br>";
+
+    echo "Here power is = $power <br><br>";
+
+    $result2=pow($base2,$power2);
+
+    echo "The result is = $result2";
+?>
