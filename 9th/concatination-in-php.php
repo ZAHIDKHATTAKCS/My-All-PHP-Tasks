3603de19e0d08b3d0110a7cebb828707dc712c1f
zahidkhattak <zahidkhattakcs@gmail.com>
2023-09-08 12:30:24 +0500

    Today also learn about Concatination in php

---
+++ 9th/concatination-in-php.php
@@ -0,0 +1,17 @@
+<?php
+
+// concatination is used for combine two string with each other
+
+$firstname="Zahid";
+
+$lastname="Khattak";
+
+$rollno=474;
+
+$degree="BSCS";
+
+$age=22;
+
+echo 'My First Name is '.$firstname.'. My Last Name is '.$lastname.'. And my roll number is '.$rollno.'. I am the student of '.$degree.'. and i am '.$age.' Years old.';
+
+?>
