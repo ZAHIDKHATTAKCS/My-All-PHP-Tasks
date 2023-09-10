0cd1af3889339c196b37ab4b9ae2f8086925d7e3
zahidkhattak <zahidkhattakcs@gmail.com>
2023-09-10 10:24:30 +0500

    Learn about ternary operator means that learn about if else in short way

---
+++ 10th/ternary-operators.php
@@ -0,0 +1,9 @@
+<?php
+
+// now learning ternary operators in php
+
+$a=3;
+// here it is called ternary operator it's same as if else if true run that one if not true run that one
+echo ($a==3)? '<h1>yes it is equal</h1>' : '<h1>oh no its not equal</h1>';
+
+?>
